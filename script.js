ede8da4e8f1d461006f2e278b3699f25686d1db0
Robin Fussell <rfussell17@gmail.com>
2019-12-05 14:11:04 -0800

    refactored code

--- script.js
+++ script.js
@@ -12,79 +12,82 @@
   const workMode = document.getElementById("workId");
   const restMode = document.getElementById("restId");
 
-// sets timer to 25:00
+  // sets timer to 25:00
 
-  workMode.addEventListener('click', () => {
-    if(workMode.checked){
+  workMode.addEventListener("click", () => {
+    if (workMode.checked) {
       clearInterval(timer);
       totalSeconds = WORK_SECONDS;
       timerDisplay(totalSeconds);
     }
-  }); 
-
-//sets timer to 05:00
-
-  restMode.addEventListener('click', () => {
-    if(restMode.checked){
-    clearInterval(timer);
-    totalSeconds = REST_SECONDS;
-    timerDisplay(totalSeconds);
-    }
-  }); 
-
-  startButton.addEventListener('click', () => {
-    clearInterval(timer);
-     if (workMode.checked)
-      totalSeconds = totalSeconds;
-     if(restMode.checked)
-       totalSeconds = totalSeconds;
-      clearInterval(timer)
-      timer = setInterval(countDown, 1000); 
   });
 
-  stopButton.addEventListener('click', () => {
-    clearInterval(timer);
-  });
+  //sets timer to 05:00
 
-// resets timer to 25 or 5 minutes based on workMode or restMode
-
-  resetButton.addEventListener('click', () => {
-    if(workMode.checked){
-      totalSeconds = WORK_SECONDS;
-    } else if(restMode.checked){
+  restMode.addEventListener("click", () => {
+    if (restMode.checked) {
+      clearInterval(timer);
       totalSeconds = REST_SECONDS;
-      }
       timerDisplay(totalSeconds);
-   });
+    }
+  });
 
-// displays correctly formatted time
+  startButton.addEventListener("click", () => {
+    clearInterval(timer);
+    if (workMode.checked) totalSeconds = totalSeconds;
+    if (restMode.checked) totalSeconds = totalSeconds;
+    clearInterval(timer);
+    timer = setInterval(countDown, 1000);
+  });
 
-  function timerDisplay(totalSeconds){
+  stopButton.addEventListener("click", () => {
+    clearInterval(timer);
+  });
+
+  // resets timer to 25 or 5 minutes based on workMode or restMode
+
+  resetButton.addEventListener("click", () => {
+    if (workMode.checked) {
+      totalSeconds = WORK_SECONDS;
+    } else if (restMode.checked) {
+      totalSeconds = REST_SECONDS;
+    }
+    timerDisplay(totalSeconds);
+  });
+
+  // displays correctly formatted time
+
+  function timerDisplay(totalSeconds) {
     let refactoredMinutes;
     let refactoredSeconds;
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds - minutes * 60;
-    refactoredMinutes = minutes < 10 ? refactoredMinutes = `0${minutes}` : refactoredMinutes = `${minutes}`;
-    refactoredSeconds = seconds < 10 ? refactoredSeconds = `0${seconds}` : refactoredSeconds = `${seconds}`;
-    time.innerHTML = `${refactoredMinutes}:${refactoredSeconds}`;    
-  };
+    refactoredMinutes =
+      minutes < 10
+        ? (refactoredMinutes = `0${minutes}`)
+        : (refactoredMinutes = `${minutes}`);
+    refactoredSeconds =
+      seconds < 10
+        ? (refactoredSeconds = `0${seconds}`)
+        : (refactoredSeconds = `${seconds}`);
+    time.innerHTML = `${refactoredMinutes}:${refactoredSeconds}`;
+  }
 
-//decrement time until 00:00, plays timer sound
+  //decrement time until 00:00, plays timer sound
 
-  function countDown(){
-    totalSeconds --;
+  function countDown() {
+    totalSeconds--;
     timerDisplay(totalSeconds);
-      if(totalSeconds == 0){
-        playSound();
-        clearInterval(timer);
-        time.innerHTML = "00:00";
-      }
-    };
+    if (totalSeconds == 0) {
+      playSound();
+      clearInterval(timer);
+      time.innerHTML = "00:00";
+    }
+  }
 
-  function playSound(){
+  function playSound() {
     const audio = new Audio("sounds/shine-ding_GyZLjr4O.mp3");
     audio.play();
-    }
-  timerDisplay(totalSeconds);    
+  }
+  timerDisplay(totalSeconds);
 })();
-
